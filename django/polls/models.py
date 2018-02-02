89be36095d51395b9623302eb2b0b62e954f8108
kjs <k104agi@gmail.com>
2018-02-02 14:09:18 +0900

    Questio에 was_published 함수 구현

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -7,6 +9,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        """
+        이 Question의 게시일자가 현재일자에서 1일 전보다 이후인지를 리턴
+            (Question을 게시한지 1일이 지나지 않았는지)
+        :return:
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
