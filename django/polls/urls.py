cde63e2c2a240e65c0812c67ba3aa3a07920c5b1
kjs <k104agi@gmail.com>
2018-02-02 16:15:09 +0900

    detail.html에 투표가능한 form을 만듬

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
@@ -9,8 +10,8 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
     #re_path를 사용해 정규표현식을 구현해보세요
-    re_path(r'^$', views.index, name='index'),
-    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
-    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote')
+   # re_path(r'^$', views.index, name='index'),
+   # re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+   # re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+   # re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote')
 ]
